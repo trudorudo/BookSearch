eb583cfe527e5176f0f66f63310b952fa683ba14
Kristina <k.trudorudo@gmail.com>
2019-08-30 09:03:08 -0400

    changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import keys from "./keys"
 
 export default {
     getBooks: function(book) {
-      return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&download=epub&key=AIzaSyAKsCMZhTz13usAHFoB_PoFCk_KVxbh3OI");
+      return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&download=epub&key=" + keys.googleBooks.key);
     },
     saveBook: function(bookData) {
       return axios.post("/api/books", bookData);
---
+++ client/src/utils/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleBooks: {
+        key: "AIzaSyAKsCMZhTz13usAHFoB_PoFCk_KVxbh3OI"
+    }
+}
